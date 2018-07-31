d97fae9658f4ce3df4b9bf57e50bbaf3ebf13ad4
TheFox <krakaka088@gmail.com>
2018-07-31 17:57:28 +0300

    speed hotfix
    snake didnt speed up; now does

--- snake.js
+++ snake.js
@@ -12,7 +12,7 @@ var fruitChar = "+" //the fruit, food, whatever
 var showBorders = true //show the borders around the play field or not
 
 var speed = 0.5 //seconds before next "tick"
-var speedchange = 0.1 //the snake speeds up based on: speed-speedchange*score (leave to 0 if you dont want it to speed up)
+var speedchange = 0.02 //the snake speeds up based on: speed-speedchange*score (leave to 0 if you dont want it to speed up)
 
 //the actual code
 
@@ -37,7 +37,7 @@ process.stdin.on('keypress', (str, key) => {
 function fruit() {
     fruitpos = [Math.ceil(Math.random()*18)+1,Math.ceil(Math.random()*18)+1]
     score++
-    if(score > 1) {
+    if(score > 1 && speed-speedchange !== 0) {
         speed -= speedchange
     }
     snakepos.push(oldSnakePos[score-1].slice(0))
@@ -104,9 +104,7 @@ function render() {
     console.log("score: "+score+"\n"+" ".repeat(19)+keySymbol+" ".repeat(19))
 }
 
-fruit()
-
-tickInterval = setInterval(function() {
+function tick() {
     oldSnakePos = JSON.parse(JSON.stringify(snakepos))
     switch(keyPress) {
         case "up":
@@ -136,4 +134,8 @@ tickInterval = setInterval(function() {
         fruit()
     }
     render()
-}, speed*1000)
+    setTimeout(tick, speed*1000)
+}
+
+fruit()
+tick()
